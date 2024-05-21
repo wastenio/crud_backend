89f7fd2ca87d0b01b7b73b66690394eaf276aeee
wastenio rocha <wastenio.rocha@lynxprocess.com>
2024-05-21 19:13:17 -0300

    Alterando variaveis de conexao do firebase

--- crud/services/firebase.js
+++ crud/services/firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB7NrpZb3hkkO_Ll3ck28xD0wx3JgBZBFs",
-  authDomain: "crud-9d7eb.firebaseapp.com",
-  projectId: "crud-9d7eb",
-  storageBucket: "crud-9d7eb.appspot.com",
-  messagingSenderId: "382889413056",
-  appId: "1:382889413056:web:6715771b0c71aea468e025",
-  measurementId: "G-8KMPZ4R9MX"
+  apiKey: process.env.FIRE_API_KEY,
+  authDomain: process.env.FIRE_AUTH_DOMAIN,
+  projectId: process.env.FIRE_PROJECTID,
+  storageBucket: process.env.FIRE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIRE_MESSAGING_SENDER_ID,
+  appId: process.env.FIRE_APP_ID,
+  measurementId: process.env.FIRE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
